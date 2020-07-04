87b875bdbc4e8c63bf81e797bc11aa685a370f42
Bill Pugh <bill.pugh@gmail.com>
2020-07-04 16:42:34 -0400

    Adding some additional files

---
+++ GAEN ExplorerTests/Playground.swift
@@ -0,0 +1,21 @@
+//
+//  Playground.swift
+//  GAEN ExplorerTests
+//
+//  Created by Bill on 6/30/20.
+//  Copyright © 2020 Ninja Monkey Coders. All rights reserved.
+//
+
+import XCTest
+
+class Playground: XCTestCase {
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {}
+}
---
+++ GAEN_Explorer/Documentation/Analysis.md
@@ -0,0 +1,17 @@
+#  Analysis
+
+
+The following things get updated as part of doing analysis
+
+var experimentStatus: ExperimentStatus = .none
+var allExposures: [EncountersWithUser] = []
+@Published
+   var experimentStart: Date?
+
+   @Published
+   var experimentEnd: Date?
+   
+   
+     var diary: [DiaryEntry] = []
+
+
