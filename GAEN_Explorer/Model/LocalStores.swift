2e888b4b7345615cdb1b620939b45537b34f69ea
Bill Pugh <bill.pugh@gmail.com>
2020-07-05 15:55:34 -0400

    Fix logic reversal bug

--- GAEN_Explorer/Model/LocalStores.swift
+++ GAEN_Explorer/Model/LocalStores.swift
@@ -318,7 +318,8 @@ class LocalStore: NSObject, ObservableObject, UNUserNotificationCenterDelegate {
         DispatchQueue.main.async {
             print("Adding experiments results to local store")
             for i in 0 ..< self.allExposures.count {
-                if !self.allExposures[i].analyzed {
+                if self.allExposures[i].analyzed {
+                    print("Already analyzed exposures for \(self.allExposures[i].userName)")                 
                     continue
                 }
                 self.allExposures[i].analyzed = true
