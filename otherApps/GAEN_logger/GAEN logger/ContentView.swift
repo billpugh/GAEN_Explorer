62e0f49e60cddadca4cb16097365aaa237a65161
Bill Pugh <bill.pugh@gmail.com>
2020-11-14 13:03:01 -0500

    Lots of changes, intended for use in the wild

--- otherApps/GAEN_logger/GAEN logger/ContentView.swift
+++ otherApps/GAEN_logger/GAEN logger/ContentView.swift
@@ -10,53 +10,72 @@ import SwiftUI
 
 struct ContentView: View {
     @EnvironmentObject var localState: LocalState
+    @State private var document: ExportDocument = ExportDocument(content: ["Hello, World!"])
+    let leftWidth: CGFloat = 540
+    
+    let rightWidth: CGFloat = 250
+    @State private var isExporting: Bool = false
     var body: some View {
         ScrollView(.vertical) {
             VStack {
                 if self.localState.all.isEmpty {
-                    Text("No advertisements seen yet").frame(width: 450, alignment: .center)
+                    Text("No advertisements seen yet").frame(width: leftWidth + rightWidth, alignment: .center)
                 } else {
-                    Button(action: { self.localState.dump() }) {
+                    Button(action: { document = ExportDocument(content: self.localState.export())
+                                                               isExporting = true
+                     }) {
                         Text("Export")
                     }
-                    Text("\(self.localState.all.filter { $0.recent }.count) Active advertisers").frame(width: 450, alignment: .center)
+                    Text("\(self.localState.all.filter { $0.recent }.count) Active advertisers").frame(width: leftWidth + rightWidth, alignment: .center)
                     HStack(spacing: 20) {
-                        Text("        rssi          interval btwn pcks  ").frame(width: 450, alignment: .leading)
-                        Text("    packet seen       ").frame(width: 290, alignment: .trailing)
+                        Text("        rssi     pkt   pkts  seen for").frame(width: leftWidth, alignment: .leading)
+                        Text("    packet seen       ").frame(width: rightWidth, alignment: .trailing)
                     }
                     HStack(spacing: 20) {
-                        Text(" id  min .. max cnt     min..max     avg").frame(width: 450, alignment: .leading)
-                        Text("first          last   ").frame(width: 290, alignment: .trailing)
+                        Text(" id  min .. max  cnt   /min   seconds").frame(width: leftWidth, alignment: .leading)
+                        Text("first          last   ").frame(width: rightWidth, alignment: .trailing)
                     }
 
                     ForEach(self.localState.all.filter { $0.recent }.sorted(by: { $0 < $1 }), id: \.peripheral) { d in
                         HStack(spacing: 20) {
-                            Text("\(d.description)").frame(width: 450, alignment: .leading)
-                            Text("\(d.time) .. \(d.lastTime)").frame(width: 290, alignment: .trailing)
+                            Text("\(d.description)").frame(width: leftWidth, alignment: .leading)
+                            Text("\(d.time) .. \(d.lastTime)").frame(width: rightWidth, alignment: .trailing)
                         }
                     }
                     if (!self.localState.all.filter { !$0.recent }.isEmpty) {
                         Divider()
-                        Text("Inactive advertisers").frame(width: 450, alignment: .center)
+                        Text("\(self.localState.all.filter { !$0.recent }.count) Inactive advertisers").frame(width: leftWidth + rightWidth, alignment: .center)
                         HStack(spacing: 20) {
-                            Text("        rssi          interval btwn pcks  ").frame(width: 450, alignment: .leading)
-                            Text("    packet seen       ").frame(width: 290, alignment: .trailing)
+                            Text("        rssi     pkt   pkts  seen for").frame(width: leftWidth, alignment: .leading)
+                            Text("    packet seen       ").frame(width: rightWidth, alignment: .trailing)
                         }
                         HStack(spacing: 20) {
-                            Text(" id  min .. max cnt     min..max     avg").frame(width: 450, alignment: .leading)
-                            Text("first          last   ").frame(width: 290, alignment: .trailing)
+                            Text(" id  min .. max  cnt   /min   seconds").frame(width: leftWidth, alignment: .leading)
+                            Text("first          last   ").frame(width: rightWidth, alignment: .trailing)
                         }
 
                         ForEach(self.localState.all.filter { !$0.recent }.sorted(by: { $0 < $1 }), id: \.peripheral) { d in
                             HStack(spacing: 20) {
-                                Text("\(d.description)").frame(width: 450, alignment: .leading)
-                                Text("\(d.time) .. \(d.lastTime)").frame(width: 290, alignment: .trailing)
+                                Text("\(d.description)").frame(width: leftWidth, alignment: .leading)
+                                Text("\(d.time) .. \(d.lastTime)").frame(width: rightWidth, alignment: .trailing)
                             }
                         }
                     }
                 }
 
-            }.frame(maxWidth: 830)
+            }.frame(maxWidth: leftWidth+30+rightWidth)
+            .fileExporter(
+                 isPresented: $isExporting,
+                 document: document,
+                 contentType: .commaSeparatedText,
+                 defaultFilename: "gaenLog.csv"
+             ) { result in
+                 if case .success = result {
+                     print("Export success")
+                 } else {
+                    print("Export success")
+                 }
+             }
 
         }.padding().font(.system(.body, design: .monospaced))
     }
--- otherApps/GAEN_logger/GAEN logger/Scanner.swift
+++ otherApps/GAEN_logger/GAEN logger/Scanner.swift
@@ -30,15 +30,48 @@ extension String.StringInterpolation {
     }
 }
 
-struct GAEN_device: Identifiable, Comparable {
+class AttnWindow {
+    static let maxAttn: Int = 89
+    static let period: TimeInterval = 60
+    static let minPerPeriod: Int = 10
+    static let width: Int = 5
+    static let minAttn: Int = 60
+    var startedAt: Date = Date()
+    var currentCount: Int = 0
+    var periodsOccuped: Int = 0
+    static func window(attn: Int)  -> Int {
+        return max(0, (attn - AttnWindow.minAttn) / AttnWindow.width)
+    }
+    static func firstAttn(window: Int) -> Int {
+        return window * AttnWindow.width + AttnWindow.minAttn
+    }
+    static func lastAttn(window: Int) -> Int {
+        return firstAttn(window: window) + AttnWindow.width - 1
+    }
+    func count(_ now: Date) {
+        let timeSinceLast = now.timeIntervalSince(startedAt)
+        if (timeSinceLast > AttnWindow.period || currentCount == 0) {
+            // new interval
+            if currentCount > AttnWindow.minPerPeriod {
+                periodsOccuped += 1
+            }
+            currentCount = 1
+            startedAt = now
+        } else {
+            currentCount += 1
+        }
+    }
+    
+}
+struct GAEN_device: Identifiable {
     static var nextId: Int = 0
     static func getNextId() -> Int {
         nextId += 1
         return nextId
     }
-
+    
     let id: Int = GAEN_device.getNextId()
-
+    
     static func < (lhs: GAEN_device, rhs: GAEN_device) -> Bool {
         switch (lhs.recent, rhs.recent) {
         case (true, false):
@@ -51,27 +84,27 @@ struct GAEN_device: Identifiable, Comparable {
             return lhs.id < rhs.id
         }
     }
-
+    
     static let timeFormat: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateStyle = .none
         formatter.timeStyle = .medium
         return formatter
     }()
-
+    
     typealias intervalType = Int
     static func timeStamp() -> intervalType {
         Int(mach_continuous_time() / 1_000_000)
     }
-
+    
     var time: String {
         Self.timeFormat.string(from: created)
     }
-
+    
     var lastTime: String {
         Self.timeFormat.string(from: lastDate)
     }
-
+    
     var analyzed = false
     let peripheral: CBPeripheral
     let created = Date()
@@ -85,21 +118,27 @@ struct GAEN_device: Identifiable, Comparable {
     var allPeriods: [Int] = []
     var minAttn: Int
     var maxAttn: Int
-    init(peripheral: CBPeripheral, attn: Int) {
+    var windows: [AttnWindow]
+    init(peripheral: CBPeripheral) {
         self.peripheral = peripheral
-        minAttn = attn
-        maxAttn = attn
-        allValues = [attn]
+        minAttn = 100
+        maxAttn = 0
+        allValues = []
+        var myWindows :  [AttnWindow] = []
+        for _ in 0 ... AttnWindow.window(attn: AttnWindow.maxAttn)  {
+            myWindows.append( AttnWindow() )
+        }
+        windows = myWindows
     }
-
+    
     var age: TimeInterval {
         Date().timeIntervalSince(lastDate)
     }
-
+    
     var recent: Bool {
         age < 30
     }
-
+    
     func median(_ a: ArraySlice<Int>) -> Double {
         // print("\(a.startIndex) ..< \(a.endIndex)")
         let sorted = a.sorted()
@@ -114,56 +153,73 @@ struct GAEN_device: Identifiable, Comparable {
         // print("\(result) is median of \(sorted) and \(a)")
         return result
     }
-
+    
     static let scanWidth = 11
-
+    
     static let samplePositions = [5, 25, 40, 50, 60, 75, 95]
-
-    func sample(_ a: [Any]) -> String {
-        Self.samplePositions.map { p in "\(a[a.count * p / 100])" }.joined(separator: ", ")
+    
+    func sample(_ a: [Double]) -> String {
+        return sample( a.map { Int ($0)})
     }
-
+    
+    func sample(_ a: [Int]) -> String {
+        Self.samplePositions.map { p in "\(a[a.count * p / 100], nf3)" }.joined(separator: ", ")
+    }
+    
+    
+    var minutesAt: [Int] {
+    return windows.map { $0.periodsOccuped }
+    }
+    var minutesAtString: String {
+        return minutesAt.map { "\($0, nf3)" }.joined(separator: ",")
+    }
+    
     var description: String {
-        "\(id, nf3)  \(RSSI)  \(count, nf3)  \(period)  \(duration, nf)"
+        "\(id, nf3)  \(RSSI)   \(count, nf3)  \(packetsPerMinute, nf)     \((lastSeen - firstSeen)/1000, nf),  \(minutesAtString)"
     }
-
-    func dump() {
+    
+                                      //11:24:06 AM,     0,     3,        59,     42,  42,  42,  42,  42,  42,  42
+    static let exportHeader : String = "    started,  mins, count, pckts/min,    \(samplePositions.map{ "\($0, nf3)" }.joined(separator: ", "))"
+    var export : String {
         let sorted = allValues.sorted()
-        print ("\(time), \(Int((lastSeen - firstSeen)/60000)), \(sorted.count), \(sample(sorted))")
+        return "\(time), \(Int((lastSeen - firstSeen)/60000), nf), \(sorted.count,nf),       \(packetsPerMinute,nf3),    \(sample(sorted))"
     }
-    mutating func analyze() {
-        analyzed = true
-        if allValues.count < 100 {
-            return
-        }
-        return
-        // print("First 30 raw values \(allValues[0..<30].map { String($0) }.joined(separator: ", "))")
-        let shuffled = allValues.shuffled()
-        let scanValues = stride(from: 0, to: allValues.count - Self.scanWidth, by: 1).map { median(allValues[$0 ..< $0 + Self.scanWidth]) }.sorted()
-        let shuffledMedians = stride(from: 0, to: allValues.count - Self.scanWidth, by: 1).map { median(shuffled[$0 ..< $0 + Self.scanWidth]) }.sorted()
-        let sorted = allValues.sorted()
-        print("A \(allValues.count),  \(sample(scanValues)),  \(sample(shuffledMedians)),  \(sample(sorted)),  \(sample(allPeriods.sorted()))")
-    }
-
+    
+    
     var duration: Int {
-        count == 1 ? 0 : Int((lastSeen - firstSeen) / (count - 1))
+        count == 1 ? 0 : Int((250 + lastSeen - firstSeen) / (count - 1))
     }
-
+    
+    var packetsPerMinute: Int {
+        if count <= 1 {
+            return 0
+        }
+        return count * 60000 / Int(lastSeen - firstSeen)
+        
+    }
+    
     var RSSI: String {
         "\(minAttn, nf3) ..\(maxAttn, nf3)"
     }
-
+    
     var period: String {
         "\(minPeriod, nf)..\(maxPeriod, nf)"
     }
-
+    
     mutating func update(attn: Int) {
         
         let now = Self.timeStamp()
+        let nowDate = Date()
         let thisPeriod = now - lastSeen
         if thisPeriod < 200 {
             return
         }
+        if (attn <= AttnWindow.maxAttn) {
+            for w in AttnWindow.window(attn: attn) ..< windows.count {
+                windows[w].count(nowDate)
+            }
+        }
+       
         allPeriods.append(thisPeriod)
         minAttn = min(minAttn, attn)
         maxAttn = max(maxAttn, attn)
@@ -183,14 +239,15 @@ struct GAEN_device: Identifiable, Comparable {
 
 class LocalState: ObservableObject {
     static var shared = LocalState()
-
+    
     @Published
     var all: [GAEN_device] = []
-
-    func dump() {
-        for d in all {
-            d.dump()
-        }
+    
+    func export() -> [String]{
+        var export = all.map { $0.export }
+        export.insert(GAEN_device.exportHeader, at: 0)
+        return export
+        
     }
     func saw(didDiscover peripheral: CBPeripheral, attn: Int) {
         for i in 0 ..< all.count {
@@ -199,12 +256,10 @@ class LocalState: ObservableObject {
                 return
             }
         }
-        for i in 0 ..< all.count {
-            if !all[i].analyzed, all[i].age > 200 {
-                all[i].analyze()
-            }
-        }
-        all.append(GAEN_device(peripheral: peripheral, attn: attn))
+        
+        var newDevice = GAEN_device(peripheral: peripheral)
+        newDevice.update(attn: attn)
+        all.append(newDevice)
     }
 }
 
@@ -217,22 +272,22 @@ class Scanner: NSObject, CBCentralManagerDelegate {
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         if central.state == .poweredOn {
             print("Powered on")
-
+            
             central.scanForPeripherals(withServices: [GAENServiceCBUUID], options: [CBCentralManagerScanOptionAllowDuplicatesKey: true])
         }
     }
-
+    
     override init() {
         print("init'd")
         central = CBCentralManager(delegate: nil, queue: nil)
         super.init()
         central.delegate = self
     }
-
+    
     var central: CBCentralManager
-
+    
     func hello() {}
-
+    
     func centralManager(_: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String: Any], rssi RSSI: NSNumber) {
         let attn = 3 - RSSI.intValue
         if (attn < 35) {
@@ -241,14 +296,14 @@ class Scanner: NSObject, CBCentralManagerDelegate {
         }
         LocalState.shared.saw(didDiscover: peripheral, attn: attn)
         if false { print("Saw packet, RSSI \(RSSI) ")
-        if true {
-            for (k, v) in advertisementData {
-                print(" \(k): \(v)")
+            if true {
+                for (k, v) in advertisementData {
+                    print(" \(k): \(v)")
+                }
             }
-        }
-        print()
-        print("-------")
-        print()
+            print()
+            print("-------")
+            print()
         }
     }
 }
